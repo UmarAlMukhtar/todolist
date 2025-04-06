e51004add56d11161f768c7c2d4ee5f1ac16fc45
Umar Al Mukhtar <umar1868807@gmail.com>
2025-04-06 21:23:48 +0530

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // Changed from @vitejs/plugin-react
 
 export default defineConfig({
   plugins: [react()],
